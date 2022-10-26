779d4bd136a712b5556b340ded736d481dd064b8
cuong1211 <cuonghaha43@gmail.com>
2022-10-26 22:30:03 +0700

    first commit

---
+++ predict.py
@@ -0,0 +1,121 @@
+# import the necessary packages
+from keras.models import load_model
+from collections import deque
+import numpy as np
+import argparse
+import pickle
+import cv2
+from os import listdir
+import numpy as np
+import pickle
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import train_test_split
+from keras.layers import Input, Flatten, Dense, Dropout
+from keras.models import Model
+from keras.callbacks import ModelCheckpoint
+import matplotlib.pyplot as plt
+import random
+from keras.models import  load_model
+import sys
+ 
+cap = cv2.VideoCapture(0)
+
+# Dinh nghia class
+class_name = ['00000','10000','20000','50000']
+# ap = argparse.ArgumentParser()
+# ap.add_argument("-v", "--video", required=True)
+# args = vars(ap.parse_args())
+
+# Load model da train
+model = load_model('models/resnetmodel.h5')
+model.load_weights("weights-01-0.99.hdf5")
+
+# Khai bao queue nhan dien
+# mean = np.array([123.68, 116.779, 103.939][::1], dtype="float32")
+# Q = deque(maxlen=128)
+
+# # Khoi tao cac bien
+# i=0
+# label = "Predicting..."
+
+# # Doc video
+# vs = cv2.VideoCapture(args["video"])
+
+# while True:
+#         # Lay anh tu video
+#         ret, frame = vs.read()
+#         if not ret:
+#             break
+
+#         i+=1
+#         display = frame.copy()
+
+#         # Xu ly moi 10 frame
+#         if i%10==0:
+#                 # Resize dua vao mang
+#                 frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+#                 frame = cv2.resize(frame, (224, 224)).astype("float32")
+#                 frame -= mean
+
+#                 # Du doan va dua ra ket qua
+#                 preds = model.predict(np.expand_dims(frame, axis=0))[0]
+#                 Q.append(preds)
+
+#                 # Tinh trung binh cong
+#                 results = np.array(Q).mean(axis=0)
+
+#                 # Lay lop lon nhat va gan label
+#                 i = np.argmax(results)
+#                 label = lb.classes_[i]
+
+#         # Hien thi len video
+#         text = "I'm watching: {}".format(label)
+#         cv2.putText(display, text, (30, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+
+#         # show the output image
+#         cv2.imshow("Output", display)
+#         if cv2.waitKey(1) & 0xFF ==ord("q"):
+#                 break
+
+# vs.release()
+while(True):
+    # Capture frame-by-frame
+    #
+
+    ret, image_org = cap.read()
+    if not ret:
+        continue
+    image_org = cv2.resize(image_org, dsize=None,fx=0.5,fy=0.5)
+    # Resize
+    image = image_org.copy()
+    image = cv2.resize(image, dsize=(128, 128))
+    image = image.astype('float')*1./255
+    # Convert to tensor
+    image = np.expand_dims(image, axis=0)
+
+    # Predict
+    predict = model.predict(image)
+    print("This picture is: ", class_name[np.argmax(predict[0])], (predict[0]))
+    print(np.max(predict[0],axis=0))
+    if (np.max(predict)>=0.8) and (np.argmax(predict[0])!=0):
+
+
+        # Show image
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        org = (50, 50)
+        fontScale = 1.5
+        color = (0, 255, 0)
+        thickness = 2
+
+        cv2.putText(image_org, class_name[np.argmax(predict)], org, font,
+                    fontScale, color, thickness, cv2.LINE_AA)
+
+    cv2.imshow("Picture", image_org)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
---
+++ training.py
@@ -0,0 +1,174 @@
+from inspect import ArgSpec
+from token import LBRACE
+from keras.preprocessing.image import ImageDataGenerator
+from keras.layers.pooling import AveragePooling2D
+from keras.applications.resnet import ResNet50
+from keras.layers.core import Dropout
+from keras.layers.core import Flatten
+from keras.layers.core import Dense
+from keras.layers import Input
+from keras.models import Model
+from keras.optimizers import gradient_descent_v2
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from imutils import paths
+import matplotlib.pyplot as plt
+import numpy as np
+import argparse
+import pickle
+import cv2
+import os
+from keras.layers import Input, Flatten, Dense, Dropout
+from keras.models import Model
+from keras.callbacks import ModelCheckpoint
+import matplotlib.pyplot as plt
+import random
+from keras.preprocessing.image import ImageDataGenerator
+
+
+raw_folder = "data/"
+
+
+def save_data(raw_folder=raw_folder):
+
+    dest_size = (128, 128)
+    print("Bắt đầu xử lý ảnh...")
+
+    pixels = []
+    labels = []
+
+    # Lặp qua các folder con trong thư mục raw
+    for folder in os.listdir(raw_folder):
+        if folder != '.DS_Store':
+            print("Folder=", folder)
+            # Lặp qua các file trong từng thư mục chứa các em
+            for file in os.listdir(raw_folder + folder):
+                if file != '.DS_Store':
+                    print("File=", file)
+                    pixels.append(cv2.resize(cv2.imread(
+                        raw_folder + folder + "/" + file), dsize=(224, 224)))
+                    labels.append(folder)
+
+    pixels = np.array(pixels)
+    labels = np.array(labels)  # .reshape(-1,1)
+
+    from sklearn.preprocessing import LabelBinarizer
+    lb = LabelBinarizer()
+    labels = lb.fit_transform(labels)
+    print(labels)
+
+    file = open('pix.data', 'wb')
+    # dump information to that file
+    pickle.dump((pixels, labels), file)
+    # close the file
+    file.close()
+
+    return
+
+
+def load_data():
+    file = open('pix.data', 'rb')
+
+    # dump information to that file
+    (pixels, labels) = pickle.load(file)
+
+    # close the file
+    file.close()
+
+    print(pixels.shape)
+    print(labels.shape)
+
+    return pixels, labels
+
+
+# save_data()
+X, y = load_data()
+# random.shuffle(X)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=100)
+
+print(X_train.shape)
+print(y_train.shape)
+
+# Chia train/val theo ty le 80/20
+# (trainX, testX, trainY, testY) = train_test_split(data, labels,	test_size=0.2, stratify=labels, random_state=42)
+
+# Augment anh input
+# trainAug = ImageDataGenerator(	rotation_range=30,	zoom_range=0.15,	width_shift_range=0.2,	height_shift_range=0.2,	shear_range=0.15,	horizontal_flip=True,
+# 	fill_mode="nearest")
+
+# valAug = ImageDataGenerator()
+
+# mean = np.array([123.68, 116.779, 103.939], dtype="float32")
+# trainAug.mean = mean
+# valAug.mean = mean
+
+# # Su dung mang ResNet50 voi weight imagenet
+# baseModel = ResNet50(weights="imagenet", include_top=False,	input_tensor=Input(shape=(224, 224, 3)))
+# headModel = baseModel.output
+# headModel = AveragePooling2D(pool_size=(7, 7))(headModel)
+# headModel = Flatten(name="flatten")(headModel)
+# headModel = Dense(512, activation="relu")(headModel)
+# headModel = Dropout(0.5)(headModel)
+# headModel = Dense(len(lb.classes_), activation="softmax")(headModel)
+# model = Model(inputs=baseModel.input, outputs=headModel)
+
+# # Dong bang cac lop cua mang Resnet
+# for layer in baseModel.layers:
+# 	layer.trainable = False
+
+# opt = gradient_descent_v2(lr=1e-4, momentum=0.9, decay=1e-4 / ArgSpec["epochs"])
+# model.compile(loss="categorical_crossentropy", optimizer=opt,	metrics=["accuracy"])
+
+# # Train model
+# H = model.fit_generator(
+# 	trainAug.flow(trainX, trainY, batch_size=32),	steps_per_epoch=len(trainX) // 32,	validation_data=valAug.flow(testX, testY),	validation_steps=len(testX) // 32,
+# 	epochs=25)
+
+# # Luu model
+# model.save("models/sport.h5")
+
+# # Luu ten class
+# f = open("models/lb.pickle", "wb")
+# f.write(pickle.dumps(lb))
+# f.close()
+
+# Tao model
+model_resnet_conv = ResNet50(
+    weights="imagenet", include_top=False,	input_tensor=Input(shape=(224, 224, 3)))
+output_resnet_conv = model_resnet_conv.output
+# Them cac layer FC va Dropout
+output_resnet_conv = AveragePooling2D(pool_size=(7, 7))(output_resnet_conv)
+output_resnet_conv = Flatten(name="flatten")(output_resnet_conv)
+output_resnet_conv = Dense(512, activation="relu")(output_resnet_conv)
+output_resnet_conv = Dropout(0.5)(output_resnet_conv)
+output_resnet_conv = Dense(7, activation="softmax")(output_resnet_conv)
+
+   # Dong bang cac layer
+for layer in model_resnet_conv.layers:
+    layer.trainable = False
+# Compile
+    my_model = Model(inputs=model_resnet_conv.input,
+                     outputs=output_resnet_conv)
+    my_model.compile(loss='categorical_crossentropy',
+                     optimizer='adam', metrics=['accuracy'])
+filepath = "weights-{epoch:02d}-{val_accuracy:.2f}.hdf5"
+checkpoint = ModelCheckpoint(
+    filepath, monitor='val_accuracy', verbose=1, save_best_only=True, mode='max')
+callbacks_list = [checkpoint]
+
+trainAug = ImageDataGenerator(rotation_range=30,zoom_range=0.15,width_shift_range=0.2,height_shift_range=0.2,shear_range=0.15,horizontal_flip=True,
+                              fill_mode="nearest")
+
+valAug = ImageDataGenerator()
+
+mean = np.array([123.68, 116.779, 103.939], dtype="float32")
+trainAug.mean = mean
+valAug.mean = mean
+# construct the training image generator for data augmentation
+H = my_model.fit_generator(
+    trainAug.flow(X_train, y_train, batch_size=32),	steps_per_epoch=len(X_train) // 32,	validation_data=valAug.flow(X_test, y_test),	validation_steps=len(X_test) // 32,
+    epochs=25)
+
+my_model.save("vggmodel.h5")
